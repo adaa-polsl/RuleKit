474019de9f0654140fef5613d515afd1516f609b
pmatyszok <pmatyszok@gmail.com>
2018-09-02 16:58:16 +0200

    Added test case for interval generation on non-contionus feature space

--- adaa.analytics.rules/test/adaa/analytics/rules/logic/actions/IntersectionFinderTest.java
+++ adaa.analytics.rules/test/adaa/analytics/rules/logic/actions/IntersectionFinderTest.java
@@ -6,15 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import adaa.analytics.rules.logic.representation.Interval;
 
 public class IntersectionFinderTest {
+	
+	protected IntersectionFinder finder;
+	
+	
+	@Before
+	public void SetUp() {
+		
+		finder = new IntersectionFinder();
+	}
 
 	@Test
 	public void testCalculateAllIntersectionsOf_() {
-		IntersectionFinder finder = new IntersectionFinder();
 		
 		List<Interval> intervals = new ArrayList<Interval>();
 		intervals.add(new Interval(-1, 5, true, true));
@@ -28,6 +37,27 @@ public class IntersectionFinderTest {
 		expected.add(new Interval(5, 7, true, true));
 		expected.add(new Interval(7, 10, true, true));
 		expected.add(new Interval(10, 15, true, true));
+		expected.add(Interval.create_geq(15));
+		
+		List<Interval> actual = finder.calculateAllIntersectionsOf(intervals);
+		
+		Assert.assertArrayEquals(expected.toArray(), actual.toArray());
+	}
+	
+	@Test
+	public void testNonContionusValues() {
+		
+		List<Interval> intervals = new ArrayList<Interval>();
+		intervals.add(new Interval(-1, 5, true, true));
+		intervals.add(new Interval(7, 15, true, true));
+		
+		List<Interval> expected = new ArrayList<Interval>();
+		expected.add(Interval.create_le(-1));
+		
+		expected.add(new Interval(-1, 5, true, true));
+		expected.add(new Interval(5, 7, true, true));
+		expected.add(new Interval(7, 15, true, true));
+		
 		expected.add(Interval.create_geq(15));
 		
 		List<Interval> actual = finder.calculateAllIntersectionsOf(intervals);
